217e8deee92ee666e624b20cc925016103faeeb6
Himanshu Saxena <contact@hellohimanshu.com>
2021-01-27 20:47:58 +1030

    added new pages

---
+++ client/src/pages/Blog/Blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import "./Blog.css";
+
+function Blog() {
+    return(
+        <div>
+            <Navbar/>
+        </div>
+    );
+}
+export default Blog;
