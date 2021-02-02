866d9fc0e2e6100ccf042f1db0cc00a38ad9e63c
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-02 21:52:56 +1030

    Blog display page

---
+++ client/src/pages/JournalDetail/JournalDetail.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import Navbar from "../../components/navbar/Navbar";
+import JournalDisplay from "../../components/journalDisplay/journalDisplay";
+import Footer from "../../components/footer/footer";
+
+function JournalDetail(props) {
+
+    return(
+      <div>
+        <Navbar/>
+        <JournalDisplay id={props.match.params.id}/>
+        <Footer/>
+      </div>
+    );      
+}
+
+export default JournalDetail;
