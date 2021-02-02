6cc4fc2e0e2e425773a6e1717774608b60b4cc5f
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-02 21:42:48 +1030

    auth0 routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
@@ -8,9 +10,13 @@ import "./index.css";
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App className="appStyles" />
-  </React.StrictMode>,
+  <Router>
+    <Auth0ProviderWithHistory>
+      <React.StrictMode>
+        <App className="appStyles" />
+      </React.StrictMode>
+    </Auth0ProviderWithHistory>
+  </Router>,
   document.getElementById('root')
 );
 
