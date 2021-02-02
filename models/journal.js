9f8bcf5d5df8427b92ba392d2ee1000655daa06e
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-02 21:53:27 +1030

    updated model to add image URL

---
+++ models/journal.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const journalSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  body: { type:String, required: true },
+  imageURL: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+const Journal = mongoose.model("Journal", journalSchema);
+
+module.exports = Journal;
