e46cdb0327295ecd8b55465faf8eae7f2debd1d6
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-02 22:47:14 +1030

    update auth0 redirect uri

--- client/src/auth/auth0-provider-with-history.js
+++ client/src/auth/auth0-provider-with-history.js
@@ -16,7 +16,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={"http://localhost:3000/admin"}
+      redirectUri={"https://smb-template.herokuapp.com/admin"}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
