63a3c34b4e0dc5505db4483dc1ccef0cafc7e27b
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-02 21:40:33 +1030

    added env variables and connect mongoDB

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
 
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-
+mongoose.connect(process.env.MONGODB_URI, { w: "majority", useNewUrlParser: true, useUnifiedTopology: true  });
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
