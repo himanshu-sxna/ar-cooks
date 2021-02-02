d2266adedc94bb019217a1529356851e2a1543fc
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-02 21:50:20 +1030

    updated API methods for Blog and products

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,12 +2,25 @@ import axios from "axios";
 
 export default {
 
-    APICall: function() {
+    getJournals: function() {
        return axios({
-            url: "http://localhost:3001/api",
-            method: "POST",
+            url: "/api/journal",
+            method: "GET",
           });
-    }
+    },
+    getJournalbyID: function(id) {
+      return axios({
+           url: "/api/journal/" + id,
+           method: "GET",
+         });
+   },
+   postJournalData: function(journalData) {
+     return axios({
+       url: "/api/journal/",
+       method: "POST",
+       data: journalData
+     })
+   },
 };
   
 
