304793b94c16b63b5b57c9983322ae0d479f79f1
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-22 18:32:08 +1030

    updated snipcart routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const journalRoutes = require("./journal");
 const productRoutes = require("./product");
 const imageRoutes = require("./cloudinary");
+const snipcartRoutes = require("./snipcart");
 
 // Post routes
 router.use("/journal", journalRoutes);
 router.use("/product", productRoutes);
 router.use("/image", imageRoutes );
+router.use("/snipcart", snipcartRoutes);
 
 module.exports = router;
