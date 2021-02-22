ca2800a2ba93c06ce17f88d124b8c07f7b963b07
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-22 18:33:57 +1030

    test function to store and send session ID

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,8 +5,13 @@ import Services from "../../components/services/service";
 import Contact from "../../components/contact/contact";
 import Footer from "../../components/footer/footer";
 import "./Home.css";
+import API from "../../utils/API";
+import Cookies from 'js-cookie';
 
 function Home() {
+
+  let sessionID = Cookies.get("snipcart-cart");
+  API.sendSessionID(sessionID);
 
     return(
         <div>
