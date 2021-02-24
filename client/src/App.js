60ae33e707fd4d1a3b7a6b38dd438bb1f3da71d0
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-24 10:39:57 +1030

    add global cart state

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,23 @@ import Products from "./pages/Products/Products";
 import JournalDetail from "./pages/JournalDetail/JournalDetail";
 import AdminMain from "./pages/Admin/adminMain";
 import ProtectedRoute from "./auth/protected-route";
+import CartState from "./components/context/CartState";
 
 function App() {
 
   return (
     <Router>
-      <Switch>
-        <Route exact path="/ar-cooks" component= {Home}/>
-        <Route exact path="/home" component = {Home}/>
-        <Route exact path="/blog" component={Blog}/>
-        <Route exact path ="/products" component={Products}/>
-        <Route exact path="/blog/:id" component={JournalDetail} />
-        <ProtectedRoute exact path="/admin" component={AdminMain} />
-        <Route exact path="/" component= {Home}/>
-      </Switch>
+      <CartState>
+        <Switch>
+          <Route exact path="/ar-cooks" component= {Home}/>
+          <Route exact path="/home" component = {Home}/>
+          <Route exact path="/blog" component={Blog}/>
+          <Route exact path ="/products" component={Products}/>
+          <Route exact path="/blog/:id" component={JournalDetail} />
+          <ProtectedRoute exact path="/admin" component={AdminMain} />
+          <Route exact path="/" component= {Home}/>
+        </Switch>
+      </CartState>
     </Router>
   );
 }
