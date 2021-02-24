12e9f9c67c74b138820cbf0293f5dc2f69514676
Himanshu Saxena <contact@hellohimanshu.com>
2021-02-24 10:42:48 +1030

    updating to use Global Context

--- client/src/components/utils/ShoppingCartFAB/shoppingcratFAB.js
+++ client/src/components/utils/ShoppingCartFAB/shoppingcratFAB.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import CartContext from "../../context/cartContext";
 import { Fab } from 'react-tiny-fab';
 import { MDBIcon } from "mdbreact";
-import "./ShoppingCartFAB.css"
-
-
+import "./ShoppingCartFAB.css";
 
 function ShoppingCartFAB() {
+
+    const { displayCart, SetDisplayCart } = useContext(CartContext);
+    const [ cartItems, setCartItems ] = useState();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            const itemsCount = window.Snipcart.store.getState().cart.items.count;
+            setCartItems(itemsCount);
+            if (itemsCount > 0) {   
+                SetDisplayCart(state => !state);
+            } else if (itemsCount === 0) {
+                SetDisplayCart(false);
+            }
+        }, 1000);
+        return ()=> clearTimeout(timer);
+    });
+
+    useEffect(()=> {
+       
+    }, [displayCart]);
 
     return(
         <div className="snipcart-checkout snipcart-summary">
             <Fab
             icon={<MDBIcon icon="shopping-cart" />}
             mainButtonStyles = {{backgroundColor: "#00695c"}}
-            text={<span id="itemCount" className="snipcart-items-count"></span>}
+            text={<span id="itemCount">{cartItems}</span>}
             alwaysShowTitle={true}
             />  
         </div>
